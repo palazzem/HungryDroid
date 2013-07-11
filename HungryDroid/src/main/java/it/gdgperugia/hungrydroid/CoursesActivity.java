6770b75d27d88587298693c7dcf27820c2dfa54a
Emanuele palazzetti <info@emanuelepalazzetti.eu>
2013-07-11 20:54:42 +0200

    Created simple splashscreen

---
+++ HungryDroid/src/main/java/it/gdgperugia/hungrydroid/CoursesActivity.java
@@ -0,0 +1,11 @@
+package it.gdgperugia.hungrydroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CoursesActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_courses);
+    }
+}
--- HungryDroid/src/main/java/it/gdgperugia/hungrydroid/SplashActivity.java
+++ HungryDroid/src/main/java/it/gdgperugia/hungrydroid/SplashActivity.java
@@ -1,23 +1,53 @@
 package it.gdgperugia.hungrydroid;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
 
 public class SplashActivity extends Activity {
+    // Splash screen state
+    private boolean activated = true;
+
+    // Timeout of splash screen visualization
+    private int splashTimeout = 5000;
+    private Thread splashTread = null;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
-    }
 
+        // Thread to manage splash screen
+        splashTread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    int actualTime = 0;
+                    while (activated && (actualTime < splashTimeout)) {
+                        sleep(100);
+                        actualTime += 100;
+                    }
+                } catch (InterruptedException e) {
+                    // TODO: Use Log Class to catch and log exception
+                } finally {
+                    finish();
+                    // Start the main activity
+                    Intent mainIntent = new Intent(SplashActivity.this, CoursesActivity.class);
+                    startActivity(mainIntent);
+                }
+            }
+        };
+
+        splashTread.start();
+    }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.splash, menu);
-        return true;
+    public boolean onTouchEvent(MotionEvent event) {
+        // Close splash screen after a screen touch event
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            activated = false;
+        }
+        return super.onTouchEvent(event);
     }
-    
 }
